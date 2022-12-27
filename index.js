3b4c866663fe01d432090032d822d63e8ad4d069
Mazen El Ali <mazenelali>
2022-12-27 15:15:50 +0200

    step 3

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+app.get('/test', (req, res) => {
+  let test = {
+    status : 200,
+    message:"ok",
+  }
+    res.send(test)
+  })
+  app.get('/time', (req, res) => {
+    TIME=req.query.time
+    let time = {
+      status:200,
+      message:TIME,
+      }
+    res.send(time)
+  })
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
